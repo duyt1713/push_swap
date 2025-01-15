3c327b8ee04264d67b78e12f54e70a3ec5f1ce67
duyt1713 <tungha1310@gmail.com>
2025-01-15 23:34:34 +0200

    backup 15.1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:02:25 by duha              #+#    #+#             */
-/*   Updated: 2025/01/14 18:08:03 by duha             ###   ########.fr       */
+/*   Updated: 2025/01/15 22:10:29 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,101 @@
 #include <stdlib.h>
 #include <limits.h>
 
-void	stack_init(t_node **a, char **argv);
+t_node	**stack_init(char **args);
+void	handle_error(t_node **stack);
 
 //argc and argv checks, perform split with single argc
 //initialize stack a with stack_init
 int	main(int argc, char **argv)
 {
-	t_node	*a;
-	t_node	*b;
-	(void)b;
-	a = NULL;
-	b = NULL;
+	t_node	**stack_a = NULL;
+	t_node	**stack_b = NULL;
+	char	**split;
+	char	**split_original;
+	(void)stack_b;
+
 	if (argc < 2 || (argc == 2 && !argv[1][0]))
-		return (1);
+		return (1); // No parameters provided
 
 	if (argc == 2)
 	{
-		argv = ft_split(argv[1], ' ');
-		if (!argv)
-			return (1); //split fail, verify this check
-		stack_init(&a, argv);
+		split = ft_split(argv[1], ' ');
+		if (!split)
+			return (1); // Split failed
+
+		stack_a = stack_init(split);
+		split_original = split;
+
+		while (*split)
+			free(*split++);
+		free(split_original);
 	}
-	stack_init(&a, argv + 1);
+	else
+		stack_a = stack_init(argv + 1);
+
+	// perform push_swap
+	// free stacks a & b
+	free(stack_a);
+
 	return (0);
 }
 
 //initialize stack a, handles INT limit check
-void	stack_init(t_node **a, char **argv)
+t_node	**stack_init(char **args)
 {
 	long	value;
 	int		index;
+	t_node	**stack;
 	t_node	*new_node;
-	t_node	*first_node;
+	//t_node	*next_node;
 	t_node	*last_node;
 
-	first_node = NULL;
-	last_node = NULL;
 	index = 0;
-	while (*argv && ++index)
+	last_node = NULL;
+	stack = (t_node **)malloc(sizeof(t_node *));
+	if (!stack)
+		return (NULL);
+
+	while (*args)
 	{
-		value = ft_atol(*argv++);
-		if (value > INT_MAX || value < INT_MIN)
-			exit(EXIT_FAILURE);
+		value = ft_atol(*args++);
+		if (value > INT_MAX || value < INT_MIN) //param greater than MAXINT ot not numerica
+			handle_error(stack); //handle free here later as well
 		new_node = (t_node *)malloc(sizeof(t_node));
 		if (!new_node)
 			exit(EXIT_FAILURE);
+			//handle_error(a, NULL);
 		new_node->value = value;
-		new_node->index = index;
-		new_node->next = *a;
-		*a = new_node;
-		if (!first_node)
-			first_node = new_node;
+		new_node->index = index++;
+		new_node->next = NULL;
+		if (!*stack)
+			*stack = new_node;
+		else
+			last_node->next = new_node;
 		last_node = new_node;
 	}
-	last_node->next = first_node;
+	last_node->next = *stack;
+	return (stack);
+}
+
+void	handle_error(t_node **stack)
+{
+	t_node	*current;
+	t_node	*next;
+
+	if (stack && *stack)
+	{
+		current = *stack;
+		while (current->next != *stack)
+		{
+			next = current->next;
+			free(current);
+			current = next;
+		}
+		free(current);
+		*stack = NULL;
+	}
+	free(stack);
+	ft_printf("Error.\n");
+	exit(EXIT_FAILURE);
 }
